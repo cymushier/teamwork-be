e4e73132dbde6dd9d7c95db9d5bbed790813a867
Cyrus Mushier <mushierc@gmail.com>
2019-11-11 21:22:47 +0300

    #9 | Created the Auth middleware

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-require('dotenv').config(); // Config local .env
-
 const app = express();
 
 /**
  * Setup CORS headers
  */
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
