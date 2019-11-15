1b62f16796eca784ef64fd521f63daf6a3450338
Cyrus Mushier <mushierc@gmail.com>
2019-11-15 17:01:03 +0300

    Config to allow server param for DB

--- models/pool-manager.js
+++ models/pool-manager.js
@@ -2,6 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config()
 
 module.exports = new Pool({
+  host: process.env.DB_HOST,
+  ssl: process.env.DB_SSL,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
